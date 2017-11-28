9e859ad0d95cd433f332e5f449357e51246a8ff2
Ryan G <godfreyr2@students.rowan.edu>
2017-11-28 12:22:23 -0500

    adding Adam's changes to the get request.  Finally fixed my branch.  I am back to posting to the Head.  Had a detached head problem.

--- app/src/main/java/rowan/owlhowl/MapsActivityOwlHowl.java
+++ app/src/main/java/rowan/owlhowl/MapsActivityOwlHowl.java
@@ -120,7 +120,7 @@ public class MapsActivityOwlHowl extends FragmentActivity implements OnMapReadyC
             @Override
             public void onClick(View v) {
                 //update howls.  Initiates the Get Request to the database
-                new SendGetRequest().execute();
+                new SendGetRequest().execute(getLocation().latitude,getLocation().longitude);
                 //opens view of messages
                 Intent myIntent = new Intent(getApplicationContext(), rowan.owlhowl.List.class);
                 //b.putString("howls",howls.toString());
@@ -512,14 +512,14 @@ public class MapsActivityOwlHowl extends FragmentActivity implements OnMapReadyC
     /**
      * Gets the data from the database upon request.
      */
-    public class SendGetRequest extends AsyncTask<String, Void, JSONArray>{
-        protected JSONArray doInBackground(String... arg0){
+    public class SendGetRequest extends AsyncTask<Double, Void, JSONArray>{
+        protected JSONArray doInBackground(Double... arg0){
             HttpURLConnection myConnection = null;
             try{
                 //build URL/Get data
                 Map<String, Object> params = new LinkedHashMap<>();
-                params.put("lat", getLocation().latitude);
-                params.put("lng", getLocation().longitude);
+                params.put("lat", arg0[0]);
+                params.put("lng", arg0[1]);
                 StringBuilder URLend = new StringBuilder();
                 for (Map.Entry<String, Object> param : params.entrySet()) {
                     if (URLend.length() != 0) {
